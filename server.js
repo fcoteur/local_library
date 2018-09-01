579ad181de5e1cfb2a42290c7661d634192a781d
Glitch (boom-watchmaker) <none>
2018-09-01 05:34:46 +0000

    First commit express and db setup

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+/*
+
+
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGO_DB;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+*/
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
